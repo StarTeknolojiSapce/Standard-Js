055ca6960656fcf192a1e6669a763a78ef3bd8d0
Erçetin Güler <ercetinguler@hotmail.com>
2022-08-28 17:35:14 +0300

    Update main.tf
    Signed-off-by: Erçetin Güler <ercetinguler@hotmail.com>

--- main.tf
+++ main.tf
@@ -1,2 +1,19 @@
-organization = " StarTeknolog "
-name = "StarTeknolog-workspace"
+   # "Uzak" arka uç için yapılandırma.
+#      dünya {
+#        arka uç "uzak" { 
+#         
+#          organizasyon = "örnek organizasyon"
+#
+#          
+#          çalışma alanları {
+#            isim = "örnek çalışma alanı"
+#          }
+#       
+#    
+#
+#
+#      kaynak "null_resource" "örnek" {
+#        tetikleyici = {
+#          value = "Hiçbir şey yapmayan örnek bir kaynak!"
+#        }
+#      }
